47a1ffc5e5be0c00be2de8a1eb559158e62ad61b
Marcos Antônio Falcade <rynex.mar@hotmail.com>
2024-02-29 15:47:26 -0300

    ready: add status member count add status dnd

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,22 @@
+const { ActivityType, Guild } = require("discord.js");
+const { guildId } = require("../../config.json");
 const client = require("../../index.js");
 
 client.on("ready", (event) => {
-  console.log(`${event.user.username} esta online!`);
+
+  // Deixando o bot no status de ocupado
+  client.user.setStatus('dnd')
+
+  // Pegando o total de membros no servidor e exibindo no status do bot com um daley de 30 segundos para atualizar
+  setInterval(() => {
+    const totalMember = client.guilds.cache.get(guildId).memberCount
+    
+    client.user.setActivity({
+      name: `${totalMember} membros em nossa comunidade`,
+      type: ActivityType.Playing,
+    })
+  }, 1000 * 30)
+
+  // Apenas um log no console para saber se o bot esta online
+  console.log(`✅ ${event.user.username} esta online!`);
 });
