e3f58cb02fb7577bc3751e6e6a2b5bee63445685
Marcos Antônio Falcade <rynex.mar@hotmail.com>
2024-03-02 16:28:22 -0300

    activeMember: Add condition to get channels from the database and ignore registered channels

--- src/events/activeMember.js
+++ src/events/activeMember.js
@@ -16,23 +16,29 @@ function getRandomNumber(min, max) {
 
 // Cada mensagem enviada no servidor ele salva no banco de dados os pontos de acordo com os números passados
 client.on("messageCreate", async (messageEvent) => {
-
+  
   // Pegando o ID do usuário
-  const memberID = messageEvent.author.id;
+  const userId = messageEvent.author.id;
+
+  // Pega os canais da database
+  const getChannelsId = await (await database.activeMember.tableAsync('channelsID')).get('channels');
 
   // Caso o usuário for um bot ele simplesmente não adiciona pontos parando aqui a execução
   if (messageEvent.author.bot) return;
+  
+  // Verifica se o getChannelsId não é null e também verifica se o Id do canal esta incluso no getChannelsId
+  if (getChannelsId && getChannelsId.includes(messageEvent.channelId)) return;
 
   // Pegando o que foi armazenado na database e passando na variável
-  const getActiveMember = await (await database.activeMember.tableAsync('memberPoints')).get(memberID);
+  const getActiveMember = await (await database.activeMember.tableAsync('memberPoints')).get(userId);
 
   // Caso a variável getActiveMember não exista valor algum ainda ele set o valor como points: 0
   if (!getActiveMember) {
-    await (await database.activeMember.tableAsync('memberPoints')).set(memberID, {
+    await (await database.activeMember.tableAsync('memberPoints')).set(userId, {
       points: 0,
     });
   }
 
   // Por fim ele adiciona pontos passando a função getRandomNumber()
-  await (await database.activeMember.tableAsync('memberPoints')).add(`${memberID}.points`, getRandomNumber(1, 5));
+  await (await database.activeMember.tableAsync('memberPoints')).add(`${userId}.points`, getRandomNumber(1, 5));
 });
