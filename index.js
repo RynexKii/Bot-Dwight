0cfa9ab733e14c3154efdd9934eaea2839515853
Marcos Antônio Falcade <rynex.mar@hotmail.com>
2024-03-23 09:43:41 -0300

    index: Translated some messages for PT BR

--- index.js
+++ index.js
@@ -52,9 +52,15 @@ client.on(Events.InteractionCreate, async (interaction) => {
   } catch (error) {
     console.error(error);
     if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
+      await interaction.followUp({
+        content: "Ops... Aconteceu algo deu errado, Se este problema persistir, por favor, entre em contato com o suporte da comunidade.",
+        ephemeral: true,
+      });
     } else {
-      await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+      await interaction.reply({
+        content: "Ops... Aconteceu algo deu errado, Se este problema persistir, por favor, entre em contato com o suporte da comunidade.",
+        ephemeral: true,
+      });
     }
   }
 });
